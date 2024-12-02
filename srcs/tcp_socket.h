d6f9030ec53304e3b7bffc48e08aeba480dce92d
iphuck22 <eldeshue@naver.com>
2024-12-02 22:25:03 +0900

    feat : add creation fail exception.
    add creation failure exception for socket.
    add sockaddr_in6 size for sanity check.

--- srcs/tcp_socket.h
+++ srcs/tcp_socket.h
@@ -11,10 +11,13 @@
 #ifndef FTIRC_SRCS_TCP_SOCKET_H
 #define FTIRC_SRCS_TCP_SOCKET_H
 
+#define FTIRC_SRCS_SOCKET_REQUEST_QUEUE_SIZE 50
+
 extern "C" {
 #include <netinet/in.h>
 }
 
+#include <stdexcept>
 #include <string>
 
 typedef struct sockaddr_in6 InetSocketAddress;
@@ -25,13 +28,16 @@ class TcpSocket {
   TcpSocket(TcpSocket const &);
   TcpSocket &operator=(TcpSocket const &);
 
+  static socklen_t const kInetSocketAddrLen;
+
+  socklen_t cur_inet_sock_addr_len_;
   int const socket_fd_;
   bool const is_listen_only_;
-  InetSocketAddress inet_sock_address;
+  InetSocketAddress inet_sock_address_;
 
  public:
   TcpSocket(std::string const &port_number);  // server socket, getaddrinfo
-  TcpSocket(int const sock_fd);               // client socket, accepted socket
+  TcpSocket(int const listen_sock_fd);        // client socket, accepted socket
   ~TcpSocket();
 
   int socket_fd();
@@ -39,6 +45,11 @@ class TcpSocket {
 
   in_port_t GetPortNum();
   char const *GetIPAddress();
+
+  class SocketCreationFailError : public std::runtime_error {
+   public:
+    const char *what();
+  };
 };
 
 #endif
