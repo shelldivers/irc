7200d940f2a8a19e760bfd9ce366fba51579237b
iphuck22 <eldeshue@naver.com>
2024-12-03 12:10:23 +0900

    feat : implement IPv4/TCP socket object.
    -
    
    feat : add explicit keyword to prevent casting.

--- srcs/tcp_socket.cc
+++ srcs/tcp_socket.cc
@@ -4,6 +4,7 @@ extern "C" {
 }
 
 #include <cstdlib>
+#include <cstring>
 #include <iostream>
 
 #include "tcp_socket.h"
@@ -12,14 +13,14 @@ socklen_t const TcpSocket::kInetSocketAddrLen = sizeof(InetSocketAddress);
 
 TcpSocket::TcpSocket(std::string const &port_number) try
     : cur_inet_sock_addr_len_(kInetSocketAddrLen),
-      socket_fd_(socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP)),
+      socket_fd_(socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)),
       is_listen_only_(true) {
   if (socket_fd_ == -1) throw SocketCreationFailError();
 
-  inet_sock_address_.sin6_family = AF_INET6;
-  inet_sock_address_.sin6_flowinfo = 0;
-  inet_sock_address_.sin6_port = htons(atoi(port_number.c_str()));
-  inet_sock_address_.sin6_addr = in6addr_any;
+  memset(&inet_sock_address_, 0, sizeof(InetSocketAddress));
+  inet_sock_address_.sin_family = AF_INET;
+  inet_sock_address_.sin_port = htons(atoi(port_number.c_str()));
+  inet_sock_address_.sin_addr.s_addr = htonl(INADDR_ANY);
   if (bind(socket_fd_, reinterpret_cast<struct sockaddr *>(&inet_sock_address_),
            kInetSocketAddrLen) == -1)
     throw SocketCreationFailError();
@@ -28,12 +29,6 @@ TcpSocket::TcpSocket(std::string const &port_number) try
   const int sock_reuse_opt = 1;
   if (setsockopt(socket_fd_, SOL_SOCKET, SO_REUSEADDR, &sock_reuse_opt,
                  sizeof(sock_reuse_opt)) == -1)
-    throw SocketCreationFailError();
-
-  // enable dual stack socket
-  const int disable_v6_only_opt = 0;
-  if (setsockopt(socket_fd_, IPPROTO_IPV6, IPV6_V6ONLY, &disable_v6_only_opt,
-                 sizeof(disable_v6_only_opt)) == -1)
     throw SocketCreationFailError();
 
   if (listen(socket_fd_, FTIRC_SRCS_SOCKET_REQUEST_QUEUE_SIZE) == -1)
@@ -69,3 +64,18 @@ TcpSocket::TcpSocket(int const listen_sock_fd) try
 }
 
 TcpSocket::~TcpSocket() { close(socket_fd_); }
+
+int TcpSocket::socket_fd() { return socket_fd_; }
+
+bool TcpSocket::is_listen_only() { return is_listen_only_; }
+
+// return port number in host order
+in_port_t TcpSocket::GetPortNum() { return ntohs(inet_sock_address_.sin_port); }
+
+in_addr_t TcpSocket::GetIPAddress() {
+  return ntohl(inet_sock_address_.sin_addr.s_addr);
+}
+
+const char *TcpSocket::SocketCreationFailError::what() {
+  return "Tcp Socket Creation Failed.";
+}
